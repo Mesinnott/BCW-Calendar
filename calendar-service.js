100fa6b010e47a26d63f7506dea74018a273ab73
Michael Sinnott <mesinnott@gmail.com>
2016-10-07 17:33:04 -0600

    added fullCalendar.js

--- calendar-service.js
+++ calendar-service.js
@@ -1,11 +1,3 @@
 //communicate with the jQuery calendar plugin to get data.
 
 
-$(document).ready(function() {
-
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-        weekends: true
-    })
-
-});
