396efbea363467c1b7c42d1adb5811c089197065
Michael Sinnott <mesinnott@gmail.com>
2016-10-07 17:17:35 -0600

    calendar is running

--- calendar-service.js
+++ calendar-service.js
@@ -1,14 +1,11 @@
 //communicate with the jQuery calendar plugin to get data.
 
 
-
-
 $(document).ready(function() {
-
-    // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
+        weekends: true
     })
 
 });
