b92507919e30501f243eebdfdcc35ffb89886fe3
Clint Madariaga <clintmad21@hotmail.com>
2016-10-07 16:58:08 -0600

    Added function

--- event-service.js
+++ event-service.js
@@ -1,5 +1,57 @@
+function EventService(){
 // create events
 
+function Event(owner, type, date, time, place){
+    this.owner = owner,
+    this.type = type,
+    this.date = date,
+    this.time = time,
+    this.place = place
+
+}
+
+this.createEvent = function(){
+    owner =
+    type =
+    date =
+    time =
+    place =
+    var event = new Event(owner, type, date, time, place)
+    eventList.push(event)
+}
+
+this.createReservableEvent = function(){
+    
+}
+
+this.editEvent = function(event){
+
+}
+
+
+
+
+
+
 //reserve events
+this.reserveEvent = function(event){
+
+}
+
+
+
 
 //availability
+
+
+
+
+
+
+
+
+
+
+
+
+}
