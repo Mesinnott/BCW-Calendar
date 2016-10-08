81c728be080d29bb290db048a8814b68fcfba501
Michael Sinnott <mesinnott@gmail.com>
2016-10-07 18:03:42 -0600

    nav links

--- fullCalendar.js
+++ fullCalendar.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
-        weekends: true
+        header:{ center: 'month,agendaWeek,agendaDay, listWeek'},
+
+
+        weekends: true,
+        weekNumbers: true,
+        businessHours: true,
+        navLinks: true,
+        
     })
 
 });
