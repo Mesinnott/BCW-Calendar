c2c01b873739248f5d5d09bbe7b38778d82223ae
Jonathan Steenblik <blikone@hotmail.com>
2016-10-07 19:17:03 -0600

    Let's do this!

---
+++ events.js
@@ -0,0 +1,12 @@
+var Events = [
+    {
+        id
+        time 
+        place 
+        filled (boolean)
+        description
+        owner
+        reservable (boolean)
+        type (use as class)
+    }
+]
